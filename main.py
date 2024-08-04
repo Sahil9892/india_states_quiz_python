2a752fb0dd84d8011e2f57e795b8ddb5a5d4e691
Sahil9892 <sahildorugade09@gmail.com>
2024-08-04 11:33:34 +0530

    added functionality of saving the missed states data

--- main.py
+++ main.py
@@ -3,19 +3,33 @@ import pandas
 
 screen = turtle.Screen()
 screen.title("India States Game")
-image = "mainn\python\d25_csv\INDIA_STATES\indiaa.gif"
+image = "mainn\python\d25_csv\INDIA_STATES\india_map.gif"
 screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv("mainn\python\d25_csv\INDIA_STATES\states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
+while len(guessed_states) < 28:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/28 states guessed.", prompt="What's another state's name?\nType 'exit' to end the game.").title()
+    print(answer_state)
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
-print(answer_state)
+    if answer_state == "Exit":
+        # # To create a csv file of the states which were not guessed
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        # new_data = pandas.DataFrame(missing_states)
+        # new_data.to_csv("missed_states.csv")
+        break
 
-
-
-
-
-
-screen.exitonclick()
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
